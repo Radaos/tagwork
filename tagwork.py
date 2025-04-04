3f903e9b0402a1c94c7a325c447799e60c56527e
Radaos <75271687+Radaos@users.noreply.github.com>
2025-04-05 00:16:22 +0100

    Add files via upload
    Tag directory names also

--- tagwork.py
+++ tagwork.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 """
-Tag Zwift workout names with an ID in the form [n], where n in an integer.
-This ID indicates the group they belong to, it can be used to filter for a group in Golden Cheetah.
+Tag the name field in Zwift workouts with an ID in the form [n], where n in an integer.
+This ID indicates the group they belong to, it can be used to filter for a workout group in Golden Cheetah.
 The user will be prompted to choose a directory containing workout files to process.
 Output files are written to a directory of a similar name, with a suffix of _tagged.
 
 Author: Robert Drohan
 Copyright: Copyright 2025, Robert Drohan
 License: GPLv3
-Version: 1.0
+Version: 1.01
 Status: Release
 """
 
@@ -22,8 +22,8 @@ import xml.etree.ElementTree as ET
 
 def open_dir_dialog(user_msg):
     """Ask the user to specify a directory."""
-    initial_dir = 'C:\\'
-    selected_dir = askdirectory(title=user_msg, initialdir=initial_dir)
+    default_dir = 'C:\\'
+    selected_dir = askdirectory(title=user_msg, initialdir=default_dir)
     if selected_dir:
         return selected_dir
     exit("No directory selected.")
@@ -32,7 +32,7 @@ def open_dir_dialog(user_msg):
 def make_path(path):
     """Create a directory path if it doesn't already exist."""
     try:
-        os.makedirs(path)
+        os.makedirs(path, exist_ok=True)
     except OSError as exception:
         if exception.errno != errno.EEXIST:
             raise
@@ -47,52 +47,54 @@ def main():
     root.withdraw()
 
     # Ask the user for the source directory containing workout files
-    input_dir = open_dir_dialog("Select workout directory")
-    input_dir_parts = os.path.split(input_dir)
-    output_dir = os.path.join(input_dir_parts[0], f"{input_dir_parts[1]}_tagged")
-    make_path(output_dir)
+    in_dir_root = open_dir_dialog("Select workout directory")
+    dir_nodes = os.path.split(in_dir_root)
+    out_root_dir = os.path.join(dir_nodes[0], f"{dir_nodes[1]}_tagged")
+    make_path(out_root_dir)
 
-    for dirpath, dirnames, filenames in os.walk(input_dir, topdown=True):
+    # Walk through the directory tree
+    for cur_dir_name, subdir_list, filename_list in os.walk(in_dir_root, topdown=True):
         dir_count += 1
-        for filename in filenames:
-            input_file_path = os.path.join(dirpath, filename)
-            file_name, file_extension = os.path.splitext(filename)
+        for in_filename in filename_list:
+            in_fullpath = os.path.join(cur_dir_name, in_filename)
 
             # Process Zwift files with a .zwo or .xml extension
+            fnx, file_extension = os.path.splitext(in_filename)
             if file_extension in ['.zwo', '.xml']:
                 try:
-                    # Read the file with error handling for invalid UTF characters
-                    with open(input_file_path, "r", encoding="utf-8", errors="replace") as file:
-                        xml_content = file.read()
+                    # Open file with error handling for invalid UTF characters.
+                    with open(in_fullpath, "r", encoding="utf-8", errors="replace") as xfile:
+                        xml_content = xfile.read()
 
                     # Parse the cleaned up XML content
                     root = ET.fromstring(xml_content)
 
-                except IOError as error:
-                    print(f"[IO] I/O Error {error.errno}: {error.strerror}")
+                except (IOError, ET.ParseError) as error:
+                    print(f"Error processing file {in_fullpath}: {error}")
                     continue
-                except ET.ParseError as error:
-                    print(f"XML Parsing Error: {error}")
-                    continue
-                else:
-                    # Find and update the <name> element in the XML.
-                    name_element = root.find("name")
-                    if name_element is not None:
-                        name_element.text += f" [{dir_count}]"
-                        updated_xml = ET.tostring(root, encoding="unicode")
 
-                        # Create the output file and write the updated XML to it.
-                        relative_path = os.path.relpath(input_file_path, input_dir)
-                        output_file_path = os.path.join(output_dir, relative_path)
-                        output_file_dir = os.path.dirname(output_file_path)
-                        make_path(output_file_dir)
+                # Find and update the <name> element in the XML.
+                name_element = root.find("name")
+                if name_element is not None:
+                    # Add the group ID to the name element
+                    name_element.text += f" [{dir_count}]"
+                    updated_xml = ET.tostring(root, encoding="unicode")
 
-                        try:
-                            with open(output_file_path, "w", encoding="utf-8") as file:
-                                file.write(updated_xml)
-                            file_count += 1
-                        except IOError as error:
-                            print(f"Error writing to file {output_file_path}: {error}")
+                    # Create the output file and write the updated XML to it.
+                    relative_path = os.path.relpath(in_fullpath, in_dir_root)
+                    inter_path = os.path.join(out_root_dir, relative_path)
+                    inter_dirname = os.path.dirname(inter_path)
+                    # Create the new directory name with the suffix
+                    out_dirname = f"{inter_dirname}_[{dir_count}]"
+                    output_fullpath = os.path.join(out_dirname, in_filename)
+                    make_path(out_dirname)
+
+                    try:
+                        with open(output_fullpath, "w", encoding="utf-8") as xfile:
+                            xfile.write(updated_xml)
+                        file_count += 1
+                    except IOError as error:
+                        print(f"Error writing to file {output_fullpath}: {error}")
 
     print(f"Processed {dir_count} groups, {file_count} files.")
 
